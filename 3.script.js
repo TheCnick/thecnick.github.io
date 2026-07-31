00f3c1cdf757cb8afb8f52c4f7d26f014dd3f8f9
TheCnick <c-nick@outlook.com>
2026-07-31 01:21:23 -0700

    Fix selector for drawer background image

--- 3.script.js
+++ 3.script.js
@@ -1,7 +1,7 @@
 // This is the entire content for your script.js file
 document.addEventListener('DOMContentLoaded', () => {
     const drawerContainer = document.getElementById('drawer-container');
-    const drawerBackgroundImage = document.querySelector('.drawer-background-image');
+    const drawerBackgroundImage = document.querySelector('drawer-background-image');
 
     if (drawerBackgroundImage && drawerContainer) {
         drawerBackgroundImage.addEventListener('click', () => {
