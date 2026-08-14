0bf55edde49c8c450f7a5c82233c30b481ac7c59
TheCnick <c-nick@outlook.com>
2026-08-14 01:45:47 -0700

    Implement image zoom effect on mouse interaction
    Added zoom functionality for images on mouse move and reset on mouse leave.

--- script.js
+++ script.js
@@ -12,5 +12,46 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         drawer.classList.add('is-open');
     }, 200); // 200 milliseconds = 0.2 seconds
-}
+
+    
+var zoomBoxes = document.querySelectorAll('.SkullImage');
+    zoomBoxes.forEach(function(image) {
+  var imageCss = window.getComputedStyle(image, false),
+    imageUrl = imageCss.backgroundImage
+                       .slice(4, -1).replace(/['"]/g, '');
+  // Get the original source image
+  var imageSrc = new Image();
+  imageSrc.onload = function() {
+    var imageWidth = imageSrc.naturalWidth,
+        imageHeight = imageSrc.naturalHeight,
+        ratio = imageHeight / imageWidth;
+    // Adjust the box to fit the image and to adapt responsively
+    var percentage = ratio * 100 + '%';
+    image.style.paddingBottom = percentage;
+    // Zoom and scan on mousemove
+    image.onmousemove = function(e) {
+      // Get the width of the thumbnail
+      var boxWidth = image.clientWidth,
+          // Get the cursor position, minus element offset
+          x = e.pageX - this.offsetLeft,
+          y = e.pageY - this.offsetTop,
+          // Convert coordinates to % of elem. width & height
+          xPercent = x / (boxWidth / 100) + '%',
+          yPercent = y / (boxWidth * ratio / 100) + '%';
+      // Update styles w/actual size
+      Object.assign(image.style, {
+        backgroundPosition: xPercent + ' ' + yPercent,
+        backgroundSize: imageWidth + 'px'
+      });
+    };
+    // Reset when mouse leaves
+    image.onmouseleave = function(e) {
+      Object.assign(image.style, {
+        backgroundPosition: 'center',
+        backgroundSize: 'cover'
+      });
+    };
+  }
+  imageSrc.src = imageUrl;
+});
 });
