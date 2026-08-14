28f5ef5a998baad1892a29b4c5fdca480524c13a
TheCnick <c-nick@outlook.com>
2026-08-14 01:37:11 -0700

    Implement scroll and debounce functions in script.js
    Add scroll function to move window based on mouse position and debounce function to limit the rate of execution.

--- script.js
+++ script.js
@@ -12,4 +12,40 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         drawer.classList.add('is-open');
     }, 200); // 200 milliseconds = 0.2 seconds
+
+    window.addEventListener("mousemove", debounce(scroll));
+
+function scroll(event) {
+   var checkbox = document.getElementById("SkullImage");
+
+   if (!checkbox || !checkbox.checked) {
+      return;
+   }
+
+   var y = event.clientY;
+   var x = event.clientX;
+
+   var viewportWidth = document.documentElement.clientWidth;
+   var viewportHeight = document.documentElement.clientHeight;
+   var yPercentage = (y / viewportHeight) * (viewportHeight / 4);
+   var xPercentage = (x / viewportWidth) * (viewportWidth / 4);
+
+   window.scrollTo(xPercentage, yPercentage);
+}
+
+function debounce(func, wait, immediate) {
+   var timeout;
+   return function () {
+      var context = this,
+         args = arguments;
+      var later = function () {
+         timeout = null;
+         if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+   };
+}
 });
