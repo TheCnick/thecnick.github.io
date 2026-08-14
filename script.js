7ddc6965cd03b80bc66750211a001b1d88f7b5d2
TheCnick <c-nick@outlook.com>
2026-08-14 02:04:48 -0700

    Update image zoom and mousemove interaction
    Refactor image zoom functionality and add mousemove effect.

--- script.js
+++ script.js
@@ -14,44 +14,18 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 200); // 200 milliseconds = 0.2 seconds
 
     
-var zoomBoxes = document.querySelectorAll('.SkullImage');
-    zoomBoxes.forEach(function(image) {
-  var imageCss = window.getComputedStyle(image, false),
-    imageUrl = imageCss.backgroundImage
-                       .slice(4, -1).replace(/['"]/g, '');
-  // Get the original source image
-  var imageSrc = new Image();
-  imageSrc.onload = function() {
-    var imageWidth = imageSrc.naturalWidth,
-        imageHeight = imageSrc.naturalHeight,
-        ratio = imageHeight / imageWidth;
-    // Adjust the box to fit the image and to adapt responsively
-    var percentage = ratio * 100 + '%';
-    image.style.paddingBottom = percentage;
-    // Zoom and scan on mousemove
-    image.onmousemove = function(e) {
-      // Get the width of the thumbnail
-      var boxWidth = image.clientWidth,
-          // Get the cursor position, minus element offset
-          x = e.pageX - this.offsetLeft,
-          y = e.pageY - this.offsetTop,
-          // Convert coordinates to % of elem. width & height
-          xPercent = x / (boxWidth / 100) + '%',
-          yPercent = y / (boxWidth * ratio / 100) + '%';
-      // Update styles w/actual size
-      Object.assign(image.style, {
-        backgroundPosition: xPercent + ' ' + yPercent,
-        backgroundSize: imageWidth + 'px'
-      });
-    };
-    // Reset when mouse leaves
-    image.onmouseleave = function(e) {
-      Object.assign(image.style, {
-        backgroundPosition: 'center',
-        backgroundSize: 'cover'
-      });
-    };
-  }
-  imageSrc.src = imageUrl;
+const container = document.querySelector('.FullPage');
+const image = document.querySelector('.SkullImage');
+
+container.addEventListener('mousemove', (e) => {
+  // Calculate relative cursor position (0-100)
+  const rect = container.getBoundingClientRect();
+  const x = (e.clientX - rect.left) / rect.width * 100;
+  const y = (e.clientY - rect.top) / rect.height * 100;
+
+  // Move the image in the opposite direction
+  image.style.left = -x + '%';
+  image.style.top = -y + '%';
+});
 });
 });
